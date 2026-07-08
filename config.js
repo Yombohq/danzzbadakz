ddaaa7b90b91946284f12af19e5dc931d11609b8
Yombohq <156451311+Yombohq@users.noreply.github.com>
2026-07-09 04:29:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     BOT_TOKEN: '8574942054:AAE5Wd2CYVchdHKwyoNkmWmy349EFqA5gM0', // token bot Telegram
-    PHOTO_URL: 'https://files.catbox.moe/2wknvz.jpg', // URL foto untuk /start dan FixMerah
+    PHOTO_URL: 'https://i.ibb.co.com/gM9227h1/97b285c72ebb410db395b944c1bd03a2.jpg', // URL foto untuk /start dan FixMerah
     DEVELOPER_URL: 'https://t.me/maklowwmati', //link Developer Telegram
 };
